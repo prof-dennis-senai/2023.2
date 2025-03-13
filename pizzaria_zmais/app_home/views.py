adeef1030902d118c8b3956741ffdd50e58df2f9
Dennis Rocha <dennis0201@gmail.com>
2025-03-12 21:52:00 -0300

    tela

--- pizzaria_zmais/app_home/views.py
+++ pizzaria_zmais/app_home/views.py
@@ -2,4 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_home/pages/home.html')
+    lista_produtos = [
+        {
+            'img': 'https://pizzariazermatt.com.br/imgs/produto-pizza-sabor-quatro-queijos-tele-entrega-florianopolis-sao-jose.png',
+            'nome': 'Pizza Calabresa',
+            'descricao': 'Mussarela, calabresa e cebola.',
+            'preco': 'R$ 39,90'
+        },
+        {
+            'img': 'https://pizzariazermatt.com.br/imgs/produto-pizza-sabor-quatro-queijos-tele-entrega-florianopolis-sao-jose.png',
+            'nome': 'Pizza Quatro Queijos',
+            'descricao': 'Mussarela, provolone, parmesão e gorgonzola.',
+            'preco': 'R$ 39,90'
+        },
+        {
+            'img': 'https://pizzariazermatt.com.br/imgs/produto-pizza-sabor-quatro-queijos-tele-entrega-florianopolis-sao-jose.png',
+            'nome': 'Pizza Portuguesa',
+            'descricao': 'Mussarela, provolone, tomate, azeitona e cebola.',
+            'preco': 'R$ 39,90'
+        },
+        {
+            'img': 'https://pizzariazermatt.com.br/imgs/produto-pizza-sabor-quatro-queijos-tele-entrega-florianopolis-sao-jose.png',
+            'nome': 'Pizza Portuguesa',
+            'descricao': 'Mussarela, provolone, tomate, azeitona e cebola.',
+            'preco': 'R$ 39,90'
+        },
+        {
+            'img': 'https://pizzariazermatt.com.br/imgs/produto-pizza-sabor-quatro-queijos-tele-entrega-florianopolis-sao-jose.png',
+            'nome': 'Pizza Portuguesa',
+            'descricao': 'Mussarela, provolone, tomate, azeitona e cebola.',
+            'preco': 'R$ 39,90'
+        }
+    ]
+    return render(request, 'app_home/pages/home.html', context={'produtos': lista_produtos})
