f07297ac3dfcb636f59da2ec0930997844a6924b
Dennis Rocha <dennis0201@gmail.com>
2025-03-19 21:29:40 -0300

    commit 2 aula

--- pizzaria_zmais/app_home/urls.py
+++ pizzaria_zmais/app_home/urls.py
@@ -4,4 +4,7 @@ from app_home import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('pizza/', views.criar_pizza, name='pizza'),
+    path('listar/', views.listar_pizzas, name='listar'),
+    path('deletar/<int:id>', views.deletar_pizza, name='deletar'),
+    path('atualizar/<int:id>', views.atualizar_pizza, name='atualizar'),
 ]
--- pizzaria_zmais/app_home/views.py
+++ pizzaria_zmais/app_home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app_home.models import PizzaModel
 
 # Create your views here.
 def home(request):
@@ -34,8 +35,34 @@ def home(request):
             'preco': 'R$ 39,90'
         }
     ]
+
+    lista_produtos = PizzaModel.objects.all()
     return render(request, 'app_home/pages/home.html', context={'produtos': lista_produtos})
 
 
 def criar_pizza(request):
-    return render(request, 'app_home/pages/pizza.html')
+    if request.method == 'GET':
+        return render(request, 'app_home/pages/pizza.html')
+    
+    pizza = request.POST.get('pizza')
+    preco = request.POST.get('preco')
+    imagem = request.POST.get('imagem')
+    ingredientes = request.POST.get('ingredientes')
+    PizzaModel.objects.create(pizza=pizza, preco=preco, imagem=imagem, ingredientes=ingredientes)
+    return render(request, 'app_home/pages/pizza.html', context={'pizza': pizza})
+
+
+def listar_pizzas(request):
+    pizzas = PizzaModel.objects.all()
+    return render(request, 'app_home/pages/listar.html', context={'pizzas': pizzas})
+
+
+def deletar_pizza(request, id):
+    pizza = PizzaModel.objects.get(id=id)
+    pizza.delete()
+    return redirect('listar') 
+
+
+def atualizar_pizza(request, id):
+    pizza = PizzaModel.objects.get(id=id)
+    return render(request, 'app_home/pages/pizza.html', context={'pizza': pizza})
