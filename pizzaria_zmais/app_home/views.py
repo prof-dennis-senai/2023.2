e9ba57e6636c05f42a916e25a8fe396928c34a6d
Dennis Rocha <dennis0201@gmail.com>
2025-04-02 21:39:50 -0300

    Commit 3 aula 8

--- pizzaria_zmais/app_home/views.py
+++ pizzaria_zmais/app_home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from app_home.models import PizzaModel
 from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -9,6 +10,7 @@ def home(request):
     return render(request, 'app_home/pages/home.html', context={'produtos': lista_produtos, })
 
 
+@login_required
 def criar_pizza(request):
     if request.method == 'GET':
         return render(request, 'app_home/pages/pizza.html')
@@ -20,18 +22,18 @@ def criar_pizza(request):
     pizza = PizzaModel.objects.create(pizza=pizza, preco=preco, imagem=imagem, ingredientes=ingredientes)
     return render(request, 'app_home/pages/pizza.html', context={'pizza': pizza})
 
-
+@login_required
 def listar_pizzas(request):
     pizzas = PizzaModel.objects.all()
     return render(request, 'app_home/pages/listar.html', context={'pizzas': pizzas})
 
-
+@login_required
 def deletar_pizza(request, id):
     pizza = PizzaModel.objects.get(id=id)
     pizza.delete()
     return redirect('listar') 
 
-
+@login_required
 def atualizar_pizza(request, id):
     if request.method == 'GET':
         pizza = PizzaModel.objects.get(id=id)
